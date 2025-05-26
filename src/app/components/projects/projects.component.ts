c61f7e199cdb7764646a0d13a256fff9c136efc3
Candela Landi <landicandela01@gmail.com>
2025-05-26 17:37:30 -0300

    fix name's project

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -19,32 +19,32 @@ export class ProjectsComponent {
   projects: Project[] = [
     {
       id: '1',
-      title: 'Puma CQ-Necochea',
+      title: 'Puma CQ – Necochea',
       imageAfter: '/assets/images/work-done/necochea-cq/necochea-cq.webp'
     },
     {
       id: '2',
-      title: 'B.AGRO-Bahia Blanca',
+      title: 'B.AGRO – Bahía Blanca',
       imageAfter: '/assets/images/work-done/bahia-blanca/construction-site.webp'
     },
     {
       id: '3',
-      title: 'Azul-Sapeda',
+      title: 'Azul – Sapeda',
       imageAfter: '/assets/images/work-done/azul-sapeda/station.webp'
     },
     {
       id: '4',
-      title: 'Baradero-Costa Parana',
+      title: 'Baradero – Costa Paraná',
       imageAfter: '/assets/images/work-done/baradero-costa-parana/portada.webp'
     },
     {
       id: '5',
-      title: 'Las Toninas-Trearie',
+      title: 'Las Toninas – Trearie',
       imageAfter: '/assets/images/work-done/las-toninos-trearie/portada.webp'
     },
     {
       id: '6',
-      title: 'Zarate-Traslux',
+      title: 'Zárate – Traslux',
       imageAfter: '/assets/images/work-done/zarate-traslux/traslux.webp'
     }
   ];
