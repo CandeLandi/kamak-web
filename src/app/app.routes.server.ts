af3a0c68df5dcd20e9a7ce69acc2e94f9a9def78
Adrian Cabello <adrian.necoh@gmail.com>
2025-07-19 13:16:58 -0300

    change routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,14 +3,30 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: 'projects/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   },
   {
     path: 'admin/project/:id',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'admin/project',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'admin/dashboard',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'admin/login',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: '',
+    renderMode: RenderMode.Prerender
   },
   {
     path: '**',
-    renderMode: RenderMode.Server
+    renderMode: RenderMode.Prerender
   }
 ];
