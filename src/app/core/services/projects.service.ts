740bc0593fcdc7d58efa0f433786ecb46e8aaa21
Candela Landi <landicandela01@gmail.com>
2025-07-28 19:51:14 -0300

    fix map

--- src/app/core/services/projects.service.ts
+++ src/app/core/services/projects.service.ts
@@ -51,10 +51,21 @@ export class ProjectsService {
   }
 
     getPublicProjects(): Observable<Project[]> {
-
     const PUBLIC_CLIENT_ID = '78abe353-1728-49b0-b268-1d2ad5786317';
 
-    return this.http.get<any>(`${this.baseUrl}/projects/client/${PUBLIC_CLIENT_ID}`);
+    return this.http.get<any>(`${this.baseUrl}/projects/client/${PUBLIC_CLIENT_ID}`).pipe(
+      map(response => {
+        if (response && Array.isArray(response.data)) {
+          return response.data;
+        } else if (Array.isArray(response)) {
+          return response;
+        } else if (response && Array.isArray(response.projects)) {
+          return response.projects;
+        } else {
+          return [];
+        }
+      })
+    );
   }
 
   getPublicProjectById(id: string): Observable<Project> {
